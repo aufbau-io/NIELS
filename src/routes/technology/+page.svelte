7aa91e320685d41bafff4edb2ee93ccda056ad6b
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-07-28 13:52:23 +0100

    edit

--- src/routes/technology/+page.svelte
+++ src/routes/technology/+page.svelte
@@ -8,8 +8,8 @@
 		<img src={www} alt="graph" class="icon" />
 	</div>
   <h1>TECHNOLOGY<br>-</h1>
-  <a href="https://aufbau.io" target="_blank" rel="noreferrer"><p>Aufbau IO - intern (unpaid)</p></a>
-  <a href="https://midnight.agency" target="_blank" rel="noreferrer"><p>Midnight Agency - Senior Engineer</p></a>
+  <a href="https://aufbau.io" target="_blank" rel="noreferrer"><p>Aufbau IO - intern&nbsp;(unpaid)</p></a>
+  <a href="https://midnight.agency" target="_blank" rel="noreferrer"><p>Midnight Agency - Senior&nbsp;Engineer</p></a>
   <a href="https://iota.health" target="_blank" rel="noreferrer"><p>IOTA - CTO / Co-Founder</p></a>
 </div>
 </section>
