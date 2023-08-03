acae2a5dc5fc02403cacf079cd78560044761e52
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-03 11:56:34 +0100

    remove old code

--- src/lib/three-d/World/Environment.js
+++ src/lib/three-d/World/Environment.js
@@ -17,7 +17,7 @@ export default class Environment {
 			this.debugFolder = this.debug.ui.addFolder('environment');
 		}
 
-		this.setSunLight();
+		// this.setSunLight();
 
 		// -------------------------------------------------------------------------
 
@@ -104,77 +104,77 @@ export default class Environment {
 		document.addEventListener('mousemove', onDocumentMouseMove, false);
 	}
 
-	setSunLight() {
-		this.sunLight = new THREE.DirectionalLight('#232323', 1);
-		this.sunLight.castShadow = true;
-		this.sunLight.shadow.camera.far = 15;
-		this.sunLight.shadow.mapSize.set(1024, 1024);
-		this.sunLight.shadow.normalBias = 0.05;
-		this.sunLight.position.set(3.5, 2, -1.25);
-		this.scene.add(this.sunLight);
+	// setSunLight() {
+	// 	this.sunLight = new THREE.DirectionalLight('#232323', 1);
+	// 	this.sunLight.castShadow = true;
+	// 	this.sunLight.shadow.camera.far = 15;
+	// 	this.sunLight.shadow.mapSize.set(1024, 1024);
+	// 	this.sunLight.shadow.normalBias = 0.05;
+	// 	this.sunLight.position.set(3.5, 2, -1.25);
+	// 	this.scene.add(this.sunLight);
 
-		// Debug
-		if (this.debug.active) {
-			this.debugFolder
-				.add(this.sunLight, 'intensity')
-				.name('sunLightIntensity')
-				.min(0)
-				.max(10)
-				.step(0.001);
+	// 	// Debug
+	// 	if (this.debug.active) {
+	// 		this.debugFolder
+	// 			.add(this.sunLight, 'intensity')
+	// 			.name('sunLightIntensity')
+	// 			.min(0)
+	// 			.max(10)
+	// 			.step(0.001);
 
-			this.debugFolder
-				.add(this.sunLight.position, 'x')
-				.name('sunLightX')
-				.min(-5)
-				.max(5)
-				.step(0.001);
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'x')
+	// 			.name('sunLightX')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001);
 
-			this.debugFolder
-				.add(this.sunLight.position, 'y')
-				.name('sunLightY')
-				.min(-5)
-				.max(5)
-				.step(0.001);
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'y')
+	// 			.name('sunLightY')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001);
 
-			this.debugFolder
-				.add(this.sunLight.position, 'z')
-				.name('sunLightZ')
-				.min(-5)
-				.max(5)
-				.step(0.001);
-		}
-	}
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'z')
+	// 			.name('sunLightZ')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001);
+	// 	}
+	// }
 
-	setEnvironmentMap() {
-		this.environmentMap = {};
-		this.environmentMap.intensity = 0.4;
-		this.environmentMap.texture = this.resources.items.environmentMapTexture;
-		this.environmentMap.texture.encoding = THREE.sRGBEncoding;
+	// setEnvironmentMap() {
+	// 	this.environmentMap = {};
+	// 	this.environmentMap.intensity = 0.4;
+	// 	this.environmentMap.texture = this.resources.items.environmentMapTexture;
+	// 	this.environmentMap.texture.encoding = THREE.sRGBEncoding;
 
-		this.scene.environment = this.environmentMap.texture;
+	// 	this.scene.environment = this.environmentMap.texture;
 
-		this.environmentMap.updateMaterials = () => {
-			this.scene.traverse((child) => {
-				if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
-					child.material.envMap = this.environmentMap.texture;
-					child.material.envMapIntensity = this.environmentMap.intensity;
-					child.material.needsUpdate = true;
-				}
-			});
-		};
-		this.environmentMap.updateMaterials();
+	// 	this.environmentMap.updateMaterials = () => {
+	// 		this.scene.traverse((child) => {
+	// 			if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+	// 				child.material.envMap = this.environmentMap.texture;
+	// 				child.material.envMapIntensity = this.environmentMap.intensity;
+	// 				child.material.needsUpdate = true;
+	// 			}
+	// 		});
+	// 	};
+	// 	this.environmentMap.updateMaterials();
 
-		// Debug
-		if (this.debug.active) {
-			this.debugFolder
-				.add(this.environmentMap, 'intensity')
-				.name('envMapIntensity')
-				.min(0)
-				.max(4)
-				.step(0.001)
-				.onChange(this.environmentMap.updateMaterials);
-		}
-	}
+	// 	// Debug
+	// 	if (this.debug.active) {
+	// 		this.debugFolder
+	// 			.add(this.environmentMap, 'intensity')
+	// 			.name('envMapIntensity')
+	// 			.min(0)
+	// 			.max(4)
+	// 			.step(0.001)
+	// 			.onChange(this.environmentMap.updateMaterials);
+	// 	}
+	// }
 
 	update() {
 		this.shaderMaterial.uniforms.time.value = this.clock.getElapsedTime();
