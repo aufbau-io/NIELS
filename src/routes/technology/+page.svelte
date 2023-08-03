5ecc016c0c0e17c66b8546dbd9d288f192e24d8d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-03 11:54:35 +0100

    remote iota link

--- src/routes/technology/+page.svelte
+++ src/routes/technology/+page.svelte
@@ -10,7 +10,8 @@
   <h1>TECHNOLOGY<br>-</h1>
   <a href="https://aufbau.io" target="_blank" rel="noreferrer"><p>Aufbau IO - intern (unpaid)</p></a>
   <a href="https://midnight.agency" target="_blank" rel="noreferrer"><p>Midnight Agency - Senior Engineer</p></a>
-  <a href="https://iota.health" target="_blank" rel="noreferrer"><p>IOTA - CTO / Co-Founder</p></a>
+  <!-- <a href="https://iota.health" target="_blank" rel="noreferrer"><p>IOTA - CTO / Co-Founder</p></a> -->
+	<p>IOTA - CTO / Co-Founder</p>
 </div>
 </section>
 
