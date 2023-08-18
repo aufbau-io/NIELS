b0598fb6ba185b7631e7855b72302258b48af7b0
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-18 16:28:36 +0400

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,40 +13,26 @@
 
 		const experience = new Experience(document.querySelector('canvas.webgl'))
 
-		// ---------------------------------------------------------------------------
-		// HEIGHT
-		// ---------------------------------------------------------------------------
+		function getDeviceType() {
+			const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-		// let vh = window.innerHeight * 0.01;
-		// document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-		// window.addEventListener('resize', () => {
-		// 	let vh = window.innerHeight * 0.01;
-		// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
-		// });
-
-
-		// ---------------------------------------------------------------------------
-		// SCREEN
-		// ---------------------------------------------------------------------------
-		const ua = navigator.userAgent;
-		if (
-			/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)
-		) {
-			// phone
-			screenType.set(1);
-		} else if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-			// tablet
-			screenType.set(2);
-		} else {
-			//laptop
-			screenType.set(3);
+			if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
+				// This is a device which supports touch
+				if (width <= 767) {
+					// Mobile
+					return 1;
+				} else {
+					// Tablet
+					return 2;
+				}
+			} else {
+				// This is likely a laptop or desktop
+				return 3;
+			}
 		}
 
-		if (window.location !== window.parent.location) {
-			// The page is in an iframe
-			isIframe.set(true);
-		}
+		screenType.set(getDeviceType());
+		isIframe.set(window.location !== window.parent.location);
 	});
 </script>
 
