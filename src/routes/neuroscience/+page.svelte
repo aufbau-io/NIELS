0714dd56fd62ba3a6f9a856ea069f8db48b1ca0f
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-09-07 20:25:12 +0100

    edit

--- src/routes/neuroscience/+page.svelte
+++ src/routes/neuroscience/+page.svelte
@@ -8,8 +8,8 @@
 		<img src={graph} alt="graph" class="icon" />
 	</div> -->
   <!-- <h1>NEUROSCIENCE<br>-</h1> -->
-  <a href="https://bushlab-ucl.github.io" target="_blank" rel="noreferrer"><p>Human Electrophysiology Lab // UCL</p></a>
-  <a href="https://www.ucl.ac.uk/icn/research/research-groups/space-memory" target="_blank" rel="noreferrer"><p>Space and Memory Lab // UCL ICN</p></a>
+  <a href="https://bushlab-ucl.github.io" target="_blank" rel="noreferrer"><p>Human Electrophysiology Lab - UCL</p></a>
+  <a href="https://www.ucl.ac.uk/icn/research/research-groups/space-memory" target="_blank" rel="noreferrer"><p>Space and Memory Lab - UCL ICN</p></a>
 
 </div>
 </section>
