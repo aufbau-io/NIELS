6e127f6a497472cbce646931e0d7d2cfff646e36
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-02 02:29:01 +0000

    rmv links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -72,11 +72,16 @@
 	<a href="https://bushlab-ucl.github.io/"><p>- UCL :: Human Electrophysiology Lab :: Research Assistant</p></a>
 	<a href="https://www.mis.mpg.de/stochastic-topology-applications"><p>- Max Planck Institute for Mathematics in the Sciences :: Stochastic Topology Group :: Visiting Research Assistant</p></a>
 	<hr>
-	<h3>Interests</h3>
+		<h3>Interests</h3>
+	<p>- Space</p>
+	<p>- Structure</p>
+	<p>- Concepts</p>
+	<hr>
+	<!-- <h3>Interests</h3>
 	<p>- Space :: <a href="https://www.gutenberg.org/files/4280/4280-h/4280-h.htm">Kant</a>, <a href="https://www.grothendieckcircle.org/">Grothendieck</a>, <a href="https://www.sainsburywellcome.org/web/groups/behrens-lab">Behrens</a></p>
 	<p>- Structure :: <a href="https://www.gutenberg.org/files/5740/5740-pdf.pdf">Wittgenstein</a>, <a href="https://en.wikipedia.org/wiki/Yoneda_lemma">Yoneda</a></p>
 	<p>- Concepts :: <a href="https://plato.stanford.edu/entries/nonexistent-objects/">Meinong</a>, <a href="https://www.phil.cmu.edu/projects/carnap/editorial/latex_pdf/1928-1e%20part1.pdf">Carnap</a>,  <a href="https://link.springer.com/content/pdf/10.1016/j.thbio.2006.02.001.pdf">Jost</a></p>
-	<hr>
+	<hr> -->
 	<h3>Things</h3>
 	<a href="/Harvesting_and_Sowing.pdf"><p>- Harvesting and Sowing :: Alexander Grothendieck :: English Version :: Niels Machine Tranlation (Very Rough WIP)</p></a>
 </div>
