990b9943736746979e1a80dcfbb722f1b81e0ae6
dnhmphrs <dan@aufbau.io>
2023-12-05 13:29:17 +0000

    trim

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 
 	onMount(async () => {
 
-		const experience = new Experience(document.querySelector('canvas.webgl'))
+		new Experience(document.querySelector('canvas.webgl'))
 
 		function getDeviceType() {
 			const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -39,94 +39,17 @@
 <svelte:head>
 	<title>DANIEL HUMPHRIES</title>
 	<meta name="description" content="Daniel Humphries. Neuroscience and Mathemtics. UCL." />
-
-	<link
-	rel="preload"
-	href="/fonts/NB-Architekt-Pro-Light.woff"
-	as="font"
-	type="font/woff"
-	crossorigin="anonymous"
-/>
-
-<link
-	rel="preload"
-	href="/fonts/NB-Architekt-Pro-Bold.woff"
-	as="font"
-	type="font/woff"
-	crossorigin="anonymous"
-/>
-
-<link rel="preload" href="icons/cv.svg" as="image">
-<link rel="preload" href="icons/www.svg" as="image">
-<link rel="preload" href="icons/insta.svg" as="image">
-<link rel="preload" href="icons/mail.svg" as="image">
 </svelte:head>
 
 
 <canvas class="webgl"></canvas>
-<div class="main { $mathematics ? 'visible' : 'hidden' }" style="padding:10px;position:absolute;height:auto;width:100%;font-family:serif;display:flex;flex-flow:column;gap:2px;">
-		<h2>Daniel Niels Humphries</h2>
-		<img src="niels.jpg" style="border:solid 1px black;" width=188 />
-		<p style="letter-spacing:1.35px">Neuroscience & Mathematics</p>
-		<h3>Places</h3>
-		<a href="https://bushlab-ucl.github.io/"><p>- UCL :: Human Electrophysiology Lab :: Research Assistant</p></a>
-		<a href="https://www.mis.mpg.de/stochastic-topology-applications"><p>- Max Planck Institute for Mathematics in the Sciences :: Stochastic Topology Group :: Visiting Research Assistant</p></a>
-			<h3>Interests</h3>
-		<p>- Space</p>
-		<p>- Structure</p>
-		<p>- Representation</p>
 
-		<!-- <h3>Interests</h3>
-		<p>- Space :: <a href="https://www.gutenberg.org/files/4280/4280-h/4280-h.htm">Kant</a>, <a href="https://www.grothendieckcircle.org/">Grothendieck</a>, <a href="https://www.sainsburywellcome.org/web/groups/behrens-lab">Behrens</a></p>
-		<p>- Structure :: <a href="https://www.gutenberg.org/files/5740/5740-pdf.pdf">Wittgenstein</a>, <a href="https://en.wikipedia.org/wiki/Yoneda_lemma">Yoneda</a></p>
-		<p>- Concepts :: <a href="https://plato.stanford.edu/entries/nonexistent-objects/">Meinong</a>, <a href="https://www.phil.cmu.edu/projects/carnap/editorial/latex_pdf/1928-1e%20part1.pdf">Carnap</a>,  <a href="https://link.springer.com/content/pdf/10.1016/j.thbio.2006.02.001.pdf">Jost</a></p>
-		<hr> -->
-		<h3>Things</h3>
-		<a href="/Harvesting_and_Sowing.pdf"><p>- Harvesting and Sowing :: Alexander Grothendieck :: English Version :: Niels Machine Tranlation (Very Rough WIP)</p></a>
-			<a href="https://aufbau.io">- Aufbau :: Creative Web Engineering</a>
-</div>
-
-<!-- <div class="{ $mathematics ? 'hidden' : 'visible' }">
-	<canvas class="webgl"></canvas>
-
-	<div class="app">
-		{#if $screenType}
-		<header>
-			<Header />
-		</header>
-
-		<main>
-			<slot />
-		</main>		
-		
-		{/if}
-	</div>
-</div> -->
-
-
-
-<!-- <footer>
-	<Footer />
-</footer> -->
+<main>
+	<slot />
+</main>		
 
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		height: 100dvh;
-		/* height: calc(var(--vh, 1vh) * 100); */
-	}
-
-	.main {
-		width: 100%;
-		z-index: 1000;
-		overflow: auto;
-		max-height: 100%;
-		backdrop-filter: blur(10px);
-		border-bottom: 1px solid black;
-	}
-
 	.webgl {
 		position: absolute;
 		left: 0;
@@ -136,41 +59,5 @@
 		min-height: 100dvh;
 		width: 100%;
 		opacity: 1;
-	}
-	
-	header {
-		position: absolute;
-		top: 0;
-		width: 100%;
-	}
-
-	footer {
-		position: absolute;
-		bottom: 0;
-		width: 100%;
-		z-index: 11;
-	}
-
-	main {
-		display: flex;
-		flex-direction: column;
-		/* padding: calc(1 * var(--margin)); */
-		width: 100%;
-		height: 100%;
-	}
-
-	/* p,
-	a {
-		font-size: 13px;
-	} */
-
-	.visible {
-			opacity: 1;
-			z-index: 10;
-	}
-
-	.hidden {
-			opacity: 0;
-			z-index: -10;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,44 @@
 <script>
-		let text1="Part-Time Web Engineer"
-		let text2="Part-Time Neuroscientist"
-
-	// import graph from '$lib/images/icons/graph.svg';
-	// import www from '$lib/images/icons/www2.svg';
-	// import chat from '$lib/images/icons/chat.svg';
 </script>
 
-<section>
-	<div class="main">
-		<!-- <div class="icons">
+<main style="">
+	<h2>Daniel Niels Humphries</h2>
+	<img src="niels.jpg" style="border:solid 1px black;" width=188 />
+	<p style="letter-spacing:1.35px">Neuroscience & Mathematics</p>
+	<h3>Places</h3>
+	<a href="https://bushlab-ucl.github.io/"><p>- UCL :: Human Electrophysiology Lab :: Research Assistant</p></a>
+	<a href="https://www.mis.mpg.de/stochastic-topology-applications"><p>- Max Planck Institute for Mathematics in the Sciences :: Stochastic Topology Group :: Visiting Research Assistant</p></a>
+		<h3>Interests</h3>
+	<p>- Space</p>
+	<p>- Structure</p>
+	<p>- Representation</p>
 
-		<img src={graph} alt="graph" class="icon" />
-		<img src={www} alt="www" class="icon" />
-		<img src={chat} alt="chat" class="icon" />
-	</div> -->
-
-		<!-- <h1>DANIEL HUMPHRIES<br>-</h1> -->
-		<p>{text1}</p>
-		<p>{text2}</p>
-		<!-- <p>{text3}</p> -->
-		<a href="https://aufbau.io">
-			// aufbau //
-			<!-- <img src="/icons/cv.svg" alt="cv" class="icon" /> -->
-    </a>
-	</div>
-	
-</section>
+	<!-- <h3>Interests</h3>
+	<p>- Space :: <a href="https://www.gutenberg.org/files/4280/4280-h/4280-h.htm">Kant</a>, <a href="https://www.grothendieckcircle.org/">Grothendieck</a>, <a href="https://www.sainsburywellcome.org/web/groups/behrens-lab">Behrens</a></p>
+	<p>- Structure :: <a href="https://www.gutenberg.org/files/5740/5740-pdf.pdf">Wittgenstein</a>, <a href="https://en.wikipedia.org/wiki/Yoneda_lemma">Yoneda</a></p>
+	<p>- Concepts :: <a href="https://plato.stanford.edu/entries/nonexistent-objects/">Meinong</a>, <a href="https://www.phil.cmu.edu/projects/carnap/editorial/latex_pdf/1928-1e%20part1.pdf">Carnap</a>,  <a href="https://link.springer.com/content/pdf/10.1016/j.thbio.2006.02.001.pdf">Jost</a></p>
+	<hr> -->
+	<h3>Things</h3>
+	<a href="/Harvesting_and_Sowing.pdf"><p>- Harvesting and Sowing :: Alexander Grothendieck :: English Version :: Niels Machine Tranlation (Very Rough WIP)</p></a>
+		<a href="https://aufbau.io">- Aufbau :: Creative Web Engineering</a>
+</main>
 
 <style>
-	section {
-		padding: 0 0;
-		height: 100%;
-		width: 100%;
+	main {
+		position:absolute;
+		height:auto;
+		width:100%;
+		max-height: 100%;
 
-		display: flex;
-		flex-flow: column nowrap;
-		justify-content: center;
-
-		align-items: center;
-		text-align: center;
-
-		/* background: var(--primary-50); */
+		display:flex;
+		flex-flow:column;
+		gap:2px;
+		z-index: 1000;
+		padding:10px;
 		overflow: auto;
-	}
 
-	.main {
-		max-width: 800px;
-
-		display: flex;
-		flex-flow: column nowrap;
-		justify-content: center;
-		gap: 10px;
-
-		background: var(--background);
-		color: var(--primary);
-		border: var(--border);
-		padding:  40px 48px;
-		margin-bottom: 40px;
-	}
-
-	.main p, a {
-		font-size: 13px;
-	}
-
-	.icons {
-		display: flex;
-		flex-flow: row;
-		padding-bottom: 16px;
-	}
-
-	.icon {
-		height: 60px;
-		width: 100%;
-	}
-
-
-	@media only screen and (max-width: 768px) {
-		.main {
-			position: absolute;
-			bottom: 20px;
-			width: calc(100vw - 40px);
-			padding:  40px 20px;
-			gap: 10px;
-		}
+		font-family:serif;
+		backdrop-filter: blur(10px);
+		border-bottom: 1px solid black;
 	}
 </style>
