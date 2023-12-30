7f265f3332ab790c2d8922df572af6519a2a0301
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-30 22:09:40 +0000

    edit SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,7 +40,7 @@
 	<meta name="description" content="Welcome to my world." />
 	<meta
 		name="keywords"
-		content="Daniel Humphries, Dan Humphries, Niels, Daniel Niels, UCL, Neuroscience, Graphics, Mathematics, Words, Machien Learning, AI, Computational Psychiatry"
+		content="Daniel Humphries, Dan Humphries, D.N. Humphries, Niels, Daniel Niels, UCL, d.humphries@ucl.ac.uk, Neuroscience, Graphics, Mathematics, Words, Machien Learning, AI, Computational Psychiatry"
 	/>
 	<meta name="author" content="Daniel Humphries" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
