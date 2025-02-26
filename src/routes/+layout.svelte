38dd6d598b39910fbe223859f2cf8bd573c59816
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-02-26 15:56:03 +0000

    remove email

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -56,7 +56,7 @@
 		<div class="header">
 				<h2>Daniel Humphries</h2>
 				<!-- <p class="less-spacing">[ Niels for Art Things ]</p> -->
-				<p class="less-spacing">d.humphries@ucl.ac.uk</p>
+				<!-- <p class="less-spacing">d.humphries@ucl.ac.uk</p> -->
 				<img src="niels_bw.jpg" alt="daniel niels humphries, this is my website." width=144 height = 144 />
 				<!-- <a href="https://aufbau.io"><p>websites/apps/graphics</p></a> -->
 				<a href="https://www.kcl.ac.uk/research/disordered-systems"><p>king's mathematics</p></a>
