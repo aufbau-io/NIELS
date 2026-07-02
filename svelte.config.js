8830a653b8c06cb6633ae679a86f436065d37d35
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2026-07-02 15:59:47 +0100

    edit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
