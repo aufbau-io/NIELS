fea5b0bcd42e110005bf2c97fe72f6bbfe4a7110
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2026-07-02 16:21:22 +0100

    fix mashup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import glsl from 'vite-plugin-glsl';
 
-const config = {
-	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
-	},
-	preprocess: vitePreprocess()
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit(), glsl()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
